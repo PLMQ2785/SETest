a339b3209b30c75eb5f4f8ed63e29998bef50b5f
PLMK2785 <dnjsdlf325@gmail.com>
2025-11-10 20:47:31 +0900

    Coop Mod 1

--- VSTest/Assets/TestCode.cs
+++ VSTest/Assets/TestCode.cs
@@ -5,7 +5,7 @@ public class TestCode : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello");
+        Debug.Log("Hello, World Commit 1!");
     }
 
     // Update is called once per frame
